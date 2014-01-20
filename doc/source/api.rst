686979b403b3f718e3604f47ce9bc706354bef23
Hrobjartur Thorsteinsson <thorsteinssonh@gmail.com>
2014-01-20 16:33:02 +0000

    Docs - api documentation bug-fix.
    - api documentation bug-fix.

--- doc/source/api.rst
+++ doc/source/api.rst
@@ -1,10 +1,21 @@
 The :mod:`pydecorate` API
 ===============================
 
+Base decorator
+---------------------------
+The basic decorator interface defined here,
+usually not instantiated by the user.
+
+.. automodule:: pydecorate.DecoratorBase
+   :members:
+   :undoc-members:
+
 Aggdraw based decorator
 ---------------------------
+Antialiased implementation of the decorator,
+inheriting from the pydecorate.DecoraterBase class.
 
-.. automodule:: pydecorate
+.. automodule:: pydecorate.DecoratorAGG
    :members:
    :undoc-members:
 
--- pydecorate/DecoratorBase.py
+++ pydecorate/DecoratorBase.py
@@ -317,30 +317,3 @@ class DecoratorBase(object):
         crop=self.image.crop(box)
         comp=Image.composite(img,crop,img)
         self.image.paste(comp,box)
-
-class Decorator(DecoratorBase):
-    def add_scale(self,color_def,font=None,size=None,fill='black',
-                  outline=None, outline_width=1, bg='white',extend=False,unit='',margins=None,minortick=0.0,
-                  nan_color=(0,0,0), nan_check_color=(1,1,1), nan_check_size=0):
-        """ Todo
-        """
-        self._add_scale(color_def,font=font,size=size,fill=fill,
-                        outline=outline,outline_widht=outline_width,bg=bg,extend=extend,unit=unit,margins=margins,minortick=minortick,
-                        nan_color=nan_color, nan_check_color=nan_check_color, nan_check_size=nan_check_size)
-
-    def _load_default_font(self):
-        return ImageFont.load_default()
-
-    def add_text(self,txt,**kwargs):
-        self._add_text(txt,**kwargs)
-
-    def add_logo(self,logo_path,**kwargs):
-        self._add_logo(logo_path,**kwargs)
-        
-
-    def _get_canvas(self,image):
-        """Returns PIL image object
-        """    
-        return ImageDraw.Draw(image)
-
-                        
