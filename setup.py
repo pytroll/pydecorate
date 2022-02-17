1d4f915932bc60f07ff8dd8ba7fadf87a440b149
David Hoese <david.hoese@ssec.wisc.edu>
2022-02-17 10:32:22 -0600

    Add missing test dependencies to CI

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ try:
     # HACK: https://github.com/pypa/setuptools_scm/issues/190#issuecomment-351181286
     # Stop setuptools_scm from including all repository files
     import setuptools_scm.integration
+
     setuptools_scm.integration.find_files = lambda _: []
 except ImportError:
     pass
@@ -32,28 +33,34 @@ with open("./README.rst", "r") as fd:
     long_description = fd.read()
 
 
-setup(name='pydecorate',
-      description='Decorating PIL images: logos, texts, pallettes',
-      long_description=long_description,
-      long_description_content_type='text/x-rst',
-      author='Hrobjartur Thorsteinsson',
-      author_email='thorsteinssonh@gmail.com',
-      classifiers=["Development Status :: 4 - Beta",
-                   "Intended Audience :: Science/Research",
-                   "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python",
-                   "Topic :: Scientific/Engineering"],
-      url="https://github.com/pytroll/pydecorate",
-      license='GPLv3+',
-      packages=['pydecorate'],
-      include_package_data=True,
-      package_data={'pydecorate': ['fonts/*.ttf']},
-      install_requires=['pillow', 'aggdraw', 'numpy'],
-      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
-      scripts=[],
-      data_files=[],
-      tests_require=['pytest'],
-      python_requires='>=3.7',
-      use_scm_version={'write_to': 'pydecorate/version.py'},
-      zip_safe=False)
+setup(
+    name="pydecorate",
+    description="Decorating PIL images: logos, texts, pallettes",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author="Hrobjartur Thorsteinsson",
+    author_email="thorsteinssonh@gmail.com",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+    ],
+    url="https://github.com/pytroll/pydecorate",
+    license="GPLv3+",
+    packages=["pydecorate"],
+    include_package_data=True,
+    package_data={"pydecorate": ["fonts/*.ttf"]},
+    install_requires=["pillow", "aggdraw", "numpy"],
+    setup_requires=["setuptools_scm", "setuptools_scm_git_archive"],
+    scripts=[],
+    data_files=[],
+    python_requires=">=3.7",
+    extras_require={
+        "tests": ["pytest"],
+    },
+    use_scm_version={"write_to": "pydecorate/version.py"},
+    zip_safe=False,
+)
