98d5e47ddaa498c8ccf2b8b3853a9fe57d22edd2
David Hoese <david.hoese@ssec.wisc.edu>
2023-03-28 14:50:04 -0500

    Switch to autogenerated API docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,7 +33,12 @@ sys.path.insert(0, os.path.abspath("../../"))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinxcontrib.apidoc"]
+
+# API docs
+apidoc_module_dir = "../../pydecorate"
+apidoc_output_dir = "api"
+apidoc_separate_modules = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
