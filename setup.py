f876a01ecaa1fa1c2b095f32de4ddfe93fccaa4c
Martin Raspaud <martin.raspaud@smhi.se>
2025-10-08 10:13:53 +0200

    Relicense to Apache v2.0

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2013 Pydecorate developers
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Package building definition and script."""
 
 import sys
@@ -53,7 +36,8 @@ setup(
         "Topic :: Scientific/Engineering",
     ],
     url="https://github.com/pytroll/pydecorate",
-    license="GPLv3+",
+    license="Apache-2.0",
+    license_files=["LICENSE.txt"],
     packages=["pydecorate"],
     include_package_data=True,
     package_data={"pydecorate": ["fonts/*.ttf"]},
